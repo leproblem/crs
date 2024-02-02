05d44a83568335e4b0f0bb2c5919cc43570078fa
leproblem <nikita.krgs@gmail.com>
2024-02-02 17:11:54 +0300

    fixed the issue with not working for h != w of the image

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ from PIL import Image
 import numpy as np
 import os
 
+
 def reduce_colors(image_path, num_colors):
     img = Image.open(image_path)
+
     img_rgb = img.convert('RGB')
+
     pixels = np.array(img_rgb).reshape((-1, 3))
     _, labels, centers = kmeans(pixels, num_colors)
-    quantized_pixels = centers[labels].reshape(img_rgb.size + (3,))
-    quantized_img = Image.fromarray(np.uint8(quantized_pixels))
 
+    # Replace each pixel with its corresponding center color
+    quantized_pixels = centers[labels].reshape(img_rgb.size[1], img_rgb.size[0], 3)
+    quantized_img = Image.fromarray(np.uint8(quantized_pixels))
+    quantized_img.save('reduced_base.png')
     return quantized_img, labels, centers
 
-def kmeans(pixels, k, max_iters=100):
 
+def kmeans(pixels, k, max_iters=100):
     centers = pixels[np.random.choice(range(len(pixels)), k, replace=False)]
 
     for _ in range(max_iters):
         labels = np.argmin(np.linalg.norm(pixels[:, np.newaxis] - centers, axis=2), axis=1)
-        new_centers = np.array([pixels[labels == i].mean(axis=0) for i in range(k)])
+
+        new_centers = np.array([pixels[labels == i].mean(axis=0) if np.any(labels == i) else centers[i] for i in range(k)])
+
         if np.all(centers == new_centers):
             break
 
@@ -26,8 +33,9 @@ def kmeans(pixels, k, max_iters=100):
 
     return centers, labels, new_centers
 
-def color_save(image, labels, centers, output_folder):
 
+
+def save_individual_colors(image, labels, centers, output_folder):
     os.makedirs(output_folder, exist_ok=True)
     image_array = np.array(image)
     unique_labels, _ = np.unique(labels, return_counts=True)
@@ -36,18 +44,22 @@ def color_save(image, labels, centers, output_folder):
         single_color_pixels = np.zeros_like(image_array)
         single_color_pixels[labels.reshape(image_array.shape[:2]) == label] = centers[label]
         single_color_img = Image.fromarray(np.uint8(single_color_pixels))
-
+        
         hex_color = "#{:02x}{:02x}{:02x}".format(*map(int, centers[label]))
 
-        output_path = os.path.join(output_folder, f'{hex_color}.png')
+        output_path = os.path.join(output_folder, f'color_{hex_color}.png')
+
         single_color_img.save(output_path)
+
 
 
 if __name__ == "__main__":
     input_image_path = str(input("Image's name to split: "))
     num_colors = int(input("Enter the number of colors: "))
-    output_folder = str("Output folder: ")
+    output_folder = str(input("Output folder: "))
 
     reduced_image, color_labels, color_centers = reduce_colors(input_image_path, num_colors)
-    color_save(reduced_image, color_labels, color_centers, output_folder)
+    
+    save_individual_colors(reduced_image, color_labels, color_centers, output_folder)
+
     print('ok')
